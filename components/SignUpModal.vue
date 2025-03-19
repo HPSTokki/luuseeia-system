da7ceef8569961ea5fadcf773b81f49e02018444
HPSTokki <sorachisenpai@gmail.com>
2025-03-19 23:57:58 +0800

    START

---
+++ components/SignUpModal.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+    export default {
+        
+    }
+</script>
+
+<template>
+    <div>
+       Hi 
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,47 @@
+<script lang="ts">
+import LoginModal from '~/components/LoginModal.vue';
+import SignUpModal from '~/components/SignUpModal.vue';
+
+export default {
+    components: {
+        LoginModal,
+        SignUpModal
+    },
+    data() {
+        return {
+            isModalOpen: false,
+            isSignUpOpen: false
+        }
+    },
+    methods: {
+        openModal() {
+            this.isModalOpen = true;
+        },
+        closeModal() {
+            this.isModalOpen = false;
+        },
+        openSignUp() {
+            this.isSignUpOpen = true;
+            this.isModalOpen = false;
+        },
+        closeSignUp() {
+            this.isSignUpOpen = false;
+        }
+    }
+}
+
+</script>
+
 <template>
     <div>
-        <header>
-            Home Page
+        <header class="headerStyles">
+            <h2 class="h2Header">Home</h2>
             <nav>
-                <ul class="flex gap-4">
+                <ul class="flex gap-6 justify-center items-center">
                     <li><nuxt-link to="/">Home</nuxt-link></li>
-                    <li><nuxt-link to="/about">about</nuxt-link></li>
-                    <li><nuxt-link to="/violationrecord">Violation Record</nuxt-link></li>
+                    <li><nuxt-link to="/about">About</nuxt-link></li>
+                    <li><button @click="openSignUp">Sign Up</button></li>
+                    <li><button class="logInBtn" @click="openModal">Login</button></li>
                 </ul>
             </nav>
         </header>
@@ -16,6 +51,8 @@
         <slot>
 
         </slot>
+        <LoginModal class="logInModal" v-if="isModalOpen" @close="closeModal"/>
+        <SignUpModal v-if="isSignUpOpen" @close="closeSignUp"/>
     </div>
 </template>
 
